f874fb921d60e505671898c409151617f6e3ee25
Nathan Lewan <nlewan@harford.edu>
2022-04-05 11:22:25 -0400

    sane default vals

--- conf/config.js
+++ conf/config.js
@@ -5,11 +5,11 @@ require('dotenv').config({ path: path.join('conf','.env') });
 
 module.exports = {
     nodeServer: {
-        port: process.env.api_NODE_PORT,
-        nodeHostName: process.env.api_NODE_SERVER_HOSTNAME.toLowerCase(),
-        socketIoStatus: process.env.api_NODE_SOCKET_IO.toLowerCase(),
-        HTTP_HTTPS: process.env.api_NODE_HTTP_HTTPS.toLowerCase(),
-        HTTPS_CERT_PATH: process.env.api_NODE_HTTPS_CERT_PATH,
-        HTTPS_KEY_PATH: process.env.api_NODE_HTTPS_CERT_KEY_PATH
+        port: process.env.api_NODE_PORT || 8080,
+        nodeHostName: (process.env.api_NODE_SERVER_HOSTNAME || 'localhost').toLowerCase(),
+        socketIoStatus: (process.env.api_NODE_SOCKET_IO || 'enabled').toLowerCase(),
+        HTTP_HTTPS: (process.env.api_NODE_HTTP_HTTPS || 'http').toLowerCase(),
+        HTTPS_CERT_PATH: process.env.api_NODE_HTTPS_CERT_PATH || '',
+        HTTPS_KEY_PATH: process.env.api_NODE_HTTPS_CERT_KEY_PATH || ''
     }
 };
