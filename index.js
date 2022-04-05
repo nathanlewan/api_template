96125fe4fb953073c0423536997c0e7736919821
Nathan Lewan <nlewan@harford.edu>
2022-04-05 15:05:40 -0400

    initial working route setup

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
 const path = require('path');
 const cfg = require( path.join(__dirname, 'conf','config') );
 const initializeWebServer = require( path.join(__dirname, 'lib', 'init', 'build_node_webserver') );
-// const initializeRoutes = require( path.join(__dirname, 'lib', 'init', 'build_node_routers') )
+const initializeRoutes = require( path.join(__dirname, 'lib', 'init', 'build_node_routers') )
 
 
 let servers = initializeWebServer( cfg )
-/* 
-initializeRoutes ( cfg, servers)
-
-servers.expressApp.get( '/', ( req, res ) => {
-    res.setHeader('content-type', 'text/html');
-    res.sendFile(path.join(__dirname, 'conf', 'index.html'))
-});
-
-servers.expressApp.get( '/io.js', ( req, res ) => {
-    res.setHeader('content-type', 'application/javascript');
-    res.sendFile(path.join(__dirname, 'node_modules', 'socket.io', 'client-dist', 'socket.io.min.js'))
-});
-
-if ( servers.socketioApp != false ) {
-    servers.socketioApp.on('connection', function(socket) {
-        console.log('A user connected');
-    
-        //Whenever someone disconnects this piece of code executed
-        socket.on('disconnect', function () {
-        console.log('A user disconnected');
-        });
-    });
-} */
+initializeRoutes( cfg, servers)
