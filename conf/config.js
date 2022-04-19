8abce33a4c0a546b3a3846d9a091062f74b4ae89
Nathan Lewan <nathanlewan@gmail.com>
2022-04-18 23:01:47 -0400

    modularize config

--- conf/config.js
+++ conf/config.js
@@ -1,15 +1,17 @@
-const fs = require('fs');
 const path = require('path');
-require('dotenv').config({ path: path.join('conf','.env') });
 
+module.exports = (basePath) => {
 
-module.exports = {
-    nodeServer: {
-        port: process.env.api_NODE_PORT || 8080,
-        nodeHostName: (process.env.api_NODE_SERVER_HOSTNAME || 'localhost').toLowerCase(),
-        socketIoStatus: (process.env.api_NODE_SOCKET_IO || 'enabled').toLowerCase(),
-        HTTP_HTTPS: (process.env.api_NODE_HTTP_HTTPS || 'http').toLowerCase(),
-        HTTPS_CERT_PATH: process.env.api_NODE_HTTPS_CERT_PATH || '',
-        HTTPS_KEY_PATH: process.env.api_NODE_HTTPS_CERT_KEY_PATH || ''
+    require('dotenv').config({ path: path.join( basePath, 'conf','.env') });
+
+    return {
+        nodeServer: {
+            port: process.env.api_NODE_PORT || 8080,
+            nodeHostName: (process.env.api_NODE_SERVER_HOSTNAME || 'localhost').toLowerCase(),
+            socketIoStatus: (process.env.api_NODE_SOCKET_IO || 'enabled').toLowerCase(),
+            HTTP_HTTPS: (process.env.api_NODE_HTTP_HTTPS || 'http').toLowerCase(),
+            HTTPS_CERT_PATH: process.env.api_NODE_HTTPS_CERT_PATH || '',
+            HTTPS_KEY_PATH: process.env.api_NODE_HTTPS_CERT_KEY_PATH || ''
+        }
     }
 };
