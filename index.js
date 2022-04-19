05e8e572c854275e92ded04d027001e19b2ff8c0
Nathan Lewan <nathanlewan@gmail.com>
2022-04-18 23:12:42 -0400

    add standalone mode

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const path = require('path');
 
+if (process.env.RUN_STANDALONE ) {
+
+    let basePath = "";
+    const cfg = require( path.join(__dirname, 'conf','config') )(basePath);
+    const initializeWebServer = require( path.join(__dirname, 'lib', 'init', 'build_node_webserver') );
+    const initializeRoutes = require( path.join(__dirname, 'lib', 'init', 'build_node_routers') )
+
+    let servers = initializeWebServer( cfg )
+    initializeRoutes( cfg, servers)
+    
+}
+
 module.exports = (basePath) => {
 
     const cfg = require( path.join(__dirname, 'conf','config') )(basePath);
