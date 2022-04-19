2d91ca0723603cd48f69034188c57b1a241d99d4
Nathan Lewan <nathanlewan@gmail.com>
2022-04-18 23:01:58 -0400

    modularize server build

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const path = require('path');
-const cfg = require( path.join(__dirname, 'conf','config') );
-const initializeWebServer = require( path.join(__dirname, 'lib', 'init', 'build_node_webserver') );
-const initializeRoutes = require( path.join(__dirname, 'lib', 'init', 'build_node_routers') )
 
+module.exports = (basePath) => {
 
-let servers = initializeWebServer( cfg )
-initializeRoutes( cfg, servers)
+    const cfg = require( path.join(__dirname, 'conf','config') )(basePath);
+    const initializeWebServer = require( path.join(__dirname, 'lib', 'init', 'build_node_webserver') );
+    const initializeRoutes = require( path.join(__dirname, 'lib', 'init', 'build_node_routers') )
+
+    let servers = initializeWebServer( cfg )
+    initializeRoutes( cfg, servers)
+
+}
